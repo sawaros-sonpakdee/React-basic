7c1d95d62e73ae82431d5d0e458170ce624509ce
Sawaros Sonpakdee <62640957+sawaros-sonpakdee@users.noreply.github.com>
2023-10-26 21:01:02 +0700

    Update App.js

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
+const Title = () => <h1>โปรแกรมบัญชีรายรับ - รายจ่าย</h1>
+const Description = () => <p>บันทึกข้อมูลบัญชีในแต่ละวัน</p>
+
+
+const Item = () => <li>เงินเดือน <span> +10,000</span></li>
+
+const Transaction = () => {
+  return (
+    // <ul>
+    //   <li>ค่าเดินทาง <span>-200</span></li>
+    //   <li>เงินเดือน<span>+20,000</span></li>
+    //   <li>เงินอาหาร<span>-500</span></li>
+    // </ul>
+    <ul>
+    <Item/>
+    </ul>
+  );
+
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Welcome to React lesson
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title />
+      <Description />
+      <Transaction />
     </div>
   );
 }
