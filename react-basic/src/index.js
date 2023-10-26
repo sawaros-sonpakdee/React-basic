9ffe08154d5b1c01ad11469b075a745e84a615a0
Sawaros Sonpakdee <62640957+sawaros-sonpakdee@users.noreply.github.com>
2023-10-26 17:15:39 +0700

    Update index.js

--- react-basic/src/index.js
+++ react-basic/src/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HelloFirstComponent from './components/HelloFirstComponent'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// กาสร้าง function Component
+// function HelloComponent(){
+//     return <h1>Log in</h1>
+// }
+// ReactDOM.render(<HelloComponent/>,document.getElementById('root'));
+
+//การสร้าง class component
+// class HelloComponent extends React.Component{
+//   render(){
+//     return <h1>Hello first Component </h1>
+//   }
+// }
+ReactDOM.render(<HelloFirstComponent/>,document.getElementById('root'))
+
+//
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
