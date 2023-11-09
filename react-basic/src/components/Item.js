17e21b9ae5cbf4bf9fd36b33fc5d112b40f04d3b
Sawaros Sonpakdee <62640957+sawaros-sonpakdee@users.noreply.github.com>
2023-11-09 17:39:23 +0700

    Update Item.js

--- react-basic/src/components/Item.js
+++ react-basic/src/components/Item.js
@@ -1,9 +1,17 @@
 import './Item.css'
+import PropTypes from 'prop-types';
+
 const Item = (props) =>{
     const {title,amount} = props
     return(
         // <li>{props.title}< span>{props.amount}</span></li>
         <li>{title}< span>{amount}</span></li>
     );
+}
+
+// prop-types
+Item.propsTypes = {
+    title:PropTypes.string,
+    amount:PropTypes.number
 }
 export default Item;
