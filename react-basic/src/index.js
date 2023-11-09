073adc98ed870aa37c5fcc8fdb0a68b55bd095e0
Sawaros Sonpakdee <62640957+sawaros-sonpakdee@users.noreply.github.com>
2023-11-09 17:42:47 +0700

    Update index.js

--- react-basic/src/index.js
+++ react-basic/src/index.js
@@ -11,11 +11,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
-<<<<<<< Updated upstream
-=======
 
-
->>>>>>> Stashed changes
 
 // กาสร้าง function Component
 // function HelloComponent(){
@@ -29,11 +25,8 @@ root.render(
 //     return <h1>Hello first Component </h1>
 //   }
 // }
-<<<<<<< Updated upstream
+
 // ReactDOM.render(<App/>,document.getElementById('root'))
-=======
-//  ReactDOM.render(<App/>,document.getElementById('root'))
->>>>>>> Stashed changes
 
 
 
