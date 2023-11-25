fa3685b038c66a5ae18c9427c1568089a37187eb
Sawaros Sonpakdee <62640957+sawaros-sonpakdee@users.noreply.github.com>
2023-11-25 19:53:04 +0700

    reportcomponent Update

--- react-basic/src/components/ReportComponent.js
+++ react-basic/src/components/ReportComponent.js
@@ -1,15 +1,22 @@
 import { useContext } from "react";
 import DataContext from "../data/DataContext";
-
+import './ReportComponent.css'
 const ReportComponent=()=>{
     const {income,expense}  =  useContext(DataContext)
     return(
         <div>
-            {/* <DataContext.Consumer>
-                {context=> }
-            </DataContext.Consumer> */}
-            <p>รายรับ: {income} </p>
-            <p>รายจ่าย:{expense}</p>
+            <h4>ยอดคงเหลือ (บาท)</h4>
+            <h1>฿{income-expense}</h1>
+            <div className="report-container">
+                <div>
+                    <h4>รายได้ทั้งหมด</h4>
+                    <p className="report plus">{income}</p>
+                </div>
+                <div>
+                    <h4>รายจ่ายทั้งหมด</h4>
+                    <p className="report minus">{expense}</p>
+                </div>
+            </div>
         </div>
     ); 
 }
