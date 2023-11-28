77d8cb9279a71e027e9c52c10a9c6d0aee1d141d
Sawaros Sonpakdee <62640957+sawaros-sonpakdee@users.noreply.github.com>
2023-11-29 00:20:39 +0700

    Format number

--- react-basic/src/components/Item.js
+++ react-basic/src/components/Item.js
@@ -2,15 +2,18 @@ import './Item.css'
 import PropTypes from 'prop-types'
 
 
-
 const Item = (props) => {
     const { title, amount } = props
     const status = amount < 0 ? "expense" : "income"
     const symbol = amount < 0 ? "-" : "+"
 
+    const formatNumber =(num)=>{
+        return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g,'$1,')
+    }
+
     return (
         // <li>{props.title}< span>{props.amount}</span></li>
-        <li className={status}>{title} {status}< span>{symbol}{Math.abs(amount)}</span>
+        <li className={status}>{title} {status}< span>{symbol}{formatNumber(Math.abs(amount))}</span>
             {/* <DataContext.Consumer>
             {value=><span>{value}</span>}
             </DataContext.Consumer> */}
